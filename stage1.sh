96207a05da3cdb8e04485902c26fb706dab265d1
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-05 09:28:29 +0000

    Enclose parted commands in quotes.

--- stage1.sh
+++ stage1.sh
@@ -13,15 +13,15 @@ end-of-script
 pacman -Sy
 
 # Setup EFI and boot 
-parted -s /dev/sda mklabel gpt
-parted -s /dev/sda mkpart esp fat32 1M 1G
-parted -s /dev/sda mkpart boot ext4 1G 2G
-parted -s /dev/sda mkpart lvm ext2 2G -1
-parted -s /dev/sda name 1 esp
-parted -s /dev/sda name 2 boot
-parted -s /dev/sda name 3 lvm
-parted -s /dev/sda toggle 1 boot
-parted -s /dev/sda toggle 3 lvm
+parted -s /dev/sda "mklabel gpt"
+parted -s /dev/sda "mkpart esp fat32 1M 1G"
+parted -s /dev/sda "mkpart boot ext4 1G 2G"
+parted -s /dev/sda "mkpart lvm ext2 2G -1"
+parted -s /dev/sda "name 1 esp"
+parted -s /dev/sda "name 2 boot"
+parted -s /dev/sda "name 3 lvm"
+parted -s /dev/sda "toggle 1 boot"
+parted -s /dev/sda "toggle 3 lvm"
 
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 -F /dev/sda2
