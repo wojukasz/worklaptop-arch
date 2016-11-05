22039dd7dc7db8696a76bc14df6ac8a4343a735c
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-05 08:57:30 +0000

    Add 3rd stage to be downloaded.

--- worklaptopsetup.sh
+++ worklaptopsetup.sh
@@ -6,3 +6,4 @@
 # fetch stages of script
 wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage1.sh
 wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage2.sh
+wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage3.sh
