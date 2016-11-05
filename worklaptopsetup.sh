f088388e29a3b3c71f40174df41d07ed027655be
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-05 08:15:37 +0000

    Setup stage downloads.

--- worklaptopsetup.sh
+++ worklaptopsetup.sh
@@ -2,4 +2,5 @@
 ### Arch Workstation Provisioning ###
 
 # fetch stages of script
-
+wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage1.sh
+wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage2.sh
