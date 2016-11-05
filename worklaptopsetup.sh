d89e7181905400dd2012d6bd32338b6547c7fa3b
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-05 16:47:56 +0000

    Make scripts executable.

--- worklaptopsetup.sh
+++ worklaptopsetup.sh
@@ -7,3 +7,6 @@
 wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage1.sh
 wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage2.sh
 wget https://raw.githubusercontent.com/demon012/worklaptop-arch/master/stage3.sh
+chmod +x stage1.sh
+chmod +x stage2.sh
+chmod +x stage3.sh
