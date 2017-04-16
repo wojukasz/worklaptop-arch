c02eb651a98c7c0578c9e1698a4fb77b1cfa5ba0
Alan Jenkins <alan.jenkins@claranet.uk>
2017-04-16 12:44:57 +0100

    Cur work on disk detection.

--- stage1.sh
+++ stage1.sh
@@ -10,29 +10,36 @@ wq
 end-of-script
 
 # Update package lists
-pacman -Sy
+pacman -Sy --noconfirm jq
 
-# Setup EFI and boot 
-parted -s /dev/sda "mklabel gpt"
-parted -s /dev/sda "mkpart esp fat32 1M 1G"
-parted -s /dev/sda "mkpart boot ext4 1G 2G"
-parted -s /dev/sda "mkpart lvm ext2 2G -1"
-parted -s /dev/sda "name 1 esp"
-parted -s /dev/sda "name 2 boot"
-parted -s /dev/sda "name 3 lvm"
-parted -s /dev/sda "toggle 1 boot"
-parted -s /dev/sda "toggle 3 lvm"
+# *TODO* Disk detection
+NUM_DISKS=$(lsblk -J | jq  '.[] | length')
+for CUR_DISK in $(seq 0 "$NUM_DISKS");
+do
+    echo "${CUR_DISK}"
+done
 
-mkfs.vfat -F32 /dev/sda1
-mkfs.ext4 -F /dev/sda2
+# Setup EFI and boot
+# parted -s /dev/sda "mklabel gpt"
+# parted -s /dev/sda "mkpart esp fat32 1M 1G"
+# parted -s /dev/sda "mkpart boot ext4 1G 2G"
+# parted -s /dev/sda "mkpart lvm ext2 2G -1"
+# parted -s /dev/sda "name 1 esp"
+# parted -s /dev/sda "name 2 boot"
+# parted -s /dev/sda "name 3 lvm"
+# parted -s /dev/sda "toggle 1 boot"
+# parted -s /dev/sda "toggle 3 lvm"
 
-## end stage 1 ##
+# mkfs.vfat -F32 /dev/sda1
+# mkfs.ext4 -F /dev/sda2
+
+# ## end stage 1 ##
 
 
-# Install device-mapper cryptsetup
+# # Install device-mapper cryptsetup
 
-echo "Run these to setup the encrypted partition:"
-echo -e "\tcryptsetup luksFormat /dev/sda3"
-echo -e "\tcryptsetup open --type luks /dev/sda3 lvm"
-echo -e "\tbash stage2.sh"
+# echo "Run these to setup the encrypted partition:"
+# echo -e "\tcryptsetup luksFormat /dev/sda3"
+# echo -e "\tcryptsetup open --type luks /dev/sda3 lvm"
+# echo -e "\tbash stage2.sh"
 
